6684dcca1509068499d997062ae7aa276a8c6e8d
saeed.rahgooy <saeed.rahgooy@gmail.com>
2020-03-15 23:06:08 +0330

    S04-E01

---
+++ MadPay724.Data/DatabaseContext/MadpayDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MadPay724.Data.DatabaseContext
+{
+    class MadpayDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(local);Initial Catalog=MadPay724db;Integrated Security = True;MultipleActiveResultSets=True;");
+        }
+    }
+}
---
+++ MadPay724.Data/Models/Photo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MadPay724.Data.Models
+{
+    public class Photo : BaseEntity<string>
+    {
+        public Photo()
+        {
+            Id = Id = Guid.NewGuid().ToString();
+            DateCreated = DateTime.Now;
+            DateModified = DateTime.Now;
+        }
+        [Required]
+        public string Url { get; set; }
+        public string Description { get; set; }
+
+        public string Alt { get; set; }
+
+        [Required]
+        public bool IsMain { get; set; }
+
+        public User User { get; set; }
+
+        [Required]
+        public string UserId { get; set; }
+
+    }
+}
